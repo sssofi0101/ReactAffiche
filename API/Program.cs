2a62f173d66542ccf50c7c37e8ed521e6e55dcbe
Sofya Sokhina <55346176+sssofi0101@users.noreply.github.com>
2024-10-23 19:02:11 +0300

    Add Client app, add Activities data to UI
    Added Vite, Semantic UI React. Added Axios for requests.

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -21,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
